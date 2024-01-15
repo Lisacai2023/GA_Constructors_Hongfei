6bdd39c664918fa8c1074165dbfe95e3143b4c2e
Lisa-cai <145958405+Lisa-cai@users.noreply.github.com>
2024-01-14 23:22:08 -0800

    Add project files.

---
+++ GA_Constructors_Hongfei/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Constructors_Hongfei
+{
+    //Week2-Constructors
+    //01/14/2024
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Exporing Constructors in C# ");
+
+            //Create the an instance with constructor
+            Student s1 = new Student("Sam",1001, 1);
+            Student s2 = new Student("Ethan",1002);
+            Student s3 = new Student("Tian", 1003);
+            Console.WriteLine("The studen is " + (s1.Name) + ", ID is " + (s1.StudentID) + ", grade is " + ( s1.Grade));
+            Console.WriteLine("The studen is " + (s2.Name) + ", ID is " + (s2.StudentID) + ", grade is " + (s2.Grade));
+            Console.WriteLine("The studen is " + (s3.Name) + ", ID is " + (s3.StudentID) + ", grade is " + (s3.Grade));
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
---
+++ GA_Constructors_Hongfei/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Constructors_Hongfei
+{
+    public class Student
+    {
+        //Create field in Student class
+        string _name;
+        int _studentID;
+        int _grade;
+
+        //Create Constructiors with parameters
+        public Student(string name, int studentID, int grade) 
+        {
+            this._name = name;
+            this._studentID = studentID;
+            this._grade = grade;
+        }
+
+        //Overloading Constructors
+        public Student(string name, int studentID)
+        {
+            this._name = name;
+            this._studentID = studentID;
+            this._grade = 3;
+        }
+
+        //Properties for name, studentID, grade
+        public string Name
+        {
+            get { return _name; }
+            set { _name =value; }
+        }
+        public int StudentID
+        {
+            get { return _studentID; }
+            set { _studentID = value; }
+        }
+
+        public int Grade
+        {
+            get { return _grade; }
+            set { _grade = value; }
+        }
+
+
+    }
+}
